a9f66d16a51b30b009d4d7141b0f36ec0d154b4d
Diogenes <{ID}+{username}@users.noreply.github.com>
2022-11-20 12:53:05 +0100

    fixing docker compose and fixing some typos

--- database/script/wait-for-postgres.sh
+++ database/script/wait-for-postgres.sh
@@ -3,8 +3,6 @@
 
 set -e
 
-shift
-
 until liquibase --defaults-file=/script/liquibase.properties --search-path=/liquibase/changelog status --changelog-file=2022-11-06-create-task-table.sql; do
   >&2 echo "Postgres is unavailable - sleeping"
   sleep 10
